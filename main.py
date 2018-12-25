bc97dca8ab42730228b0fbeecc3d32b6c2390467
Citestra <markad.oussama@gmail.com>
2018-12-25 22:48:15 +0100

    First Successful transaction sent

--- main.py
+++ main.py
@@ -14,6 +14,20 @@ import requests
 
 ################################# KEY / ADDRESS GENERATION ###################################
 # https://en.bitcoin.it/wiki/Base58Check_encoding
+# Variable length Integer
+def varint(intData):
+	if intData < 0xfd:
+		return struct.pack('<B', intData)
+	elif intData < 0xffff:
+		return struct.pack('<cH', '\xfd', intData)
+	elif intData < 0xffffffff:
+		return struct.pack('<cL', '\xfe', intData)
+	else:
+		return struct.pack('<cQ', '\xff', intData)
+
+# Variable length String
+def varstr(strData):
+	return varint(len(strData)) + strData
 
 def doubleSHA256(data):
 	return hashlib.sha256(hashlib.sha256(data).digest()).digest()
@@ -69,10 +83,10 @@ def pubKeyToAddr(publicKeyBytes):
 def addressToScriptPubKey(address):
 	# OP_DUP OP_HASH160 (20 bytes) OP_EQUALVERIFY OP_CHECKSIG
 	# 76 A9 14 (20 bytes) 88AC
-	return '76a914' + binascii.hexlify(base58CheckDecoding(address)) + '88ac'	
+	return b'\x76\xa9\x14' + base58CheckDecoding(address) + b'\x88\xac'	
 
 privateKey = privateKey256()
-publicKey = privateKeyToCompressedPublicKey(privateKey)
+publicKey = privateKeyToPublicKey(privateKey)
 wifPrivateKey = privateKeyToWif(privateKey)
 address = pubKeyToAddr(publicKey)
 print binascii.hexlify(privateKey)
@@ -85,44 +99,79 @@ print '\n'
 # For a waklthrough check the answer in this thread : https://bitcoin.stackexchange.com/questions/3374/how-to-redeem-a-basic-tx
 #HEX_PREVIOUS_TRX = 'a561cf6e8d347f5e441daafcb688d70d7b332d5601d243a503bdba4345e78276'
 HEX_PREVIOUS_TRX = '9ce36975caacacce9990f3c11b5967e64feac1de956df1ad5c0c49fdadcd01da'
+PREVIOUS_TRX_ID = binascii.unhexlify(HEX_PREVIOUS_TRX)[::-1]
 RECEIVER_ADDR = '15QCoirrat6PRNunChbnyuKMXvDnjBrgP5'
-SENDER_ADDR=address
-#SENDER_ADDR = ''
-AMOUNT_TO_SEND = 0.00001000
-AMOUNT_TO_KEEP = 0.68790000 
+#SENDER_ADDR=address
+SENDER_ADDR = '1KgA9W1nUtKbJ1ToCqxk3z26g6YsbgGpp1'
+AMOUNT_TO_SEND = 0.00001226
+FEE = 0.00001000
+AMOUNT_TO_KEEP = 0.68793000
+INPUT_VALUE = 0.68795226 
 #WIF_PRIVATE_KEY= ''
-WIF_PRIVATE_KEY=wifPrivateKey
+WIF_PRIVATE_KEY=wifPrivateKy
 
 def convertBCHtoSatoshi(bch):
 	return bch * 10**8
 
 def formatOutput(output):
 	scriptPubKey, value = output
-	return (binascii.hexlify(struct.pack('<Q', value))
-		+ '%02x' % len(scriptPubKey.decode('hex'))
-		+ scriptPubKey)
+	return struct.pack('<Q', value) + varstr(scriptPubKey)
 
 def generateRawTransaction(prevOutputHash, prevOutputIdx, scriptSig, outputs):
-	return  ( '01000000' 
-			+ '01'
-			+ binascii.hexlify(prevOutputHash.decode('hex')[::-1])
-			+ binascii.hexlify(struct.pack('<L', prevOutputIdx))
-			+ '%02x' % len(scriptSig.decode('hex'))
-			+ scriptSig
-			+ 'ffffffff'
-			+ '%02x' % len(outputs)
-			+ ''.join(map(formatOutput, outputs)) 
-			+ '00000000'
+	sequence = b'\xff\xff\xff\xff'
+	lockTime = b'\x00\x00\x00\x00'
+	sighashType = b'\x41\x00\x00\x00'
+	return  ( struct.pack('<L', 1) 
+			+ doubleSHA256(prevOutputHash + struct.pack('<L', prevOutputIdx))
+			+ doubleSHA256(sequence)
+			+ prevOutputHash + struct.pack('<L', prevOutputIdx)
+			+ varstr(scriptSig)
+			+ struct.pack('<Q', convertBCHtoSatoshi(INPUT_VALUE))
+			+ sequence
+			+ doubleSHA256(b''.join(map(formatOutput, outputs))) 
+			+ lockTime
+			+ sighashType
 			)
+
+def generateRawTransaction2(prevOutputHash, prevOutputIdx, scriptSig, outputs):
+	sequence = b'\xff\xff\xff\xff'
+	lockTime = b'\x00\x00\x00\x00'
+	return  ( struct.pack('<L', 1)
+            + b'\x01'
+            + prevOutputHash + struct.pack('<L', prevOutputIdx)
+            + varstr(scriptSig)
+            + sequence
+            + struct.pack('B', len(outputs))
+            + b''.join(map(formatOutput, outputs))
+            + lockTime
+            )
+
+def sign(sk, s256):
+	while 1:
+		sig = sk.sign_digest(s256, sigencode=ecdsa.util.sigencode_der)
+		N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
+		r, s = ecdsa.util.sigdecode_der(sig, sk.curve.generator.order())
+		if s < N/2:
+			break
+	return sig
+
+def derSigToHexSig(s):
+	s, junk = ecdsa.der.remove_sequence(s)
+	assert(junk == b'')
+	x, s = ecdsa.der.remove_integer(s)
+	y, s = ecdsa.der.remove_integer(s)
+	return binascii.unhexlify(('%064x%064x' % (x, y)))
+
 def generateSignedTransaction(privateKey, prevOutputHash, prevOutputIdx, scriptPubKey, outputs):
-	trxToSign = generateRawTransaction(prevOutputHash, prevOutputIdx, scriptPubKey, outputs) + '01000000'
-	print '\n' + trxToSign + '\n'
+	trxToSign = generateRawTransaction(prevOutputHash, prevOutputIdx, scriptPubKey, outputs)
 	hashTrxToSign = doubleSHA256(trxToSign)
 	#create a public/private key pair out of the provided private key
+	pk = privateKeyToCompressedPublicKey(privateKey)
 	sk = ecdsa.SigningKey.from_string(privateKey, curve=ecdsa.SECP256k1)
+	vk = ecdsa.VerifyingKey.from_string(privateKeyToPublicKey(privateKey)[1:], curve=ecdsa.SECP256k1)
 	#sign the hash from step with the secret key
-	trxSignature = sk.sign_digest(hashTrxToSign, sigencode=ecdsa.util.sigencode_der) + '\01'
-	pk = privateKeyToPublicKey(privateKey)
+	trxSignature = sign(sk, hashTrxToSign)
+	vk.verify_digest(derSigToHexSig(trxSignature), hashTrxToSign)
 	'''
 	Construct the final scriptSig by concatenating : 
 	- One-byte script OPCODE containing the length of the DER-encoded signature plus 1 (the length of the one-byte hash code type)
@@ -130,22 +179,22 @@ def generateSignedTransaction(privateKey, prevOutputHash, prevOutputIdx, scriptP
 	- One-byte script OPCODE containing the length of the public key
 	- The actual public key
 	'''
-	scriptSig = binascii.hexlify(chr(len(trxSignature)) + trxSignature) + binascii.hexlify(chr(len(pk)) + pk)
-	print scriptSig
-	signedTrx = generateRawTransaction(prevOutputHash, prevOutputIdx, scriptSig, outputs)
+	scriptSig = varstr(trxSignature + b'\x41') + varstr(pk)
+	hexdump(scriptSig)
+	signedTrx = generateRawTransaction2(prevOutputHash, prevOutputIdx, scriptSig, outputs)
 	return signedTrx
 
 def makeTransaction(WIFPrivateKey, prevTrxHash, prevOutputIdx, senderPublicKey, receivers):
 	#convert WIF to private key
 	privateKey = wifToPrivateKey(WIFPrivateKey)
-	print binascii.hexlify(privateKey)
 	signedTrx = generateSignedTransaction(privateKey, prevTrxHash, prevOutputIdx, senderPublicKey, receivers)
-	print 'Signed Transaction : ' + signedTrx
+	print 'Signed Transaction : ' + binascii.hexlify(signedTrx)
 	return signedTrx
 
 outputs = [(addressToScriptPubKey(RECEIVER_ADDR), convertBCHtoSatoshi(AMOUNT_TO_SEND)), (addressToScriptPubKey(SENDER_ADDR), convertBCHtoSatoshi(AMOUNT_TO_KEEP))]
 
-signedTrx = makeTransaction(WIF_PRIVATE_KEY, HEX_PREVIOUS_TRX, 1, addressToScriptPubKey(SENDER_ADDR), outputs)
+scriptPubKey = b'\x76\xa9\x14' + binascii.unhexlify('ccda1f2cd7f6240fdae4a070fd758ef57542371a') + b'\x88\xac'
+signedTrx = makeTransaction(WIF_PRIVATE_KEY, PREVIOUS_TRX_ID, 1, scriptPubKey, outputs)
 
 ################################# SEND TRANSACTION FOR MINING  ###################################
 # https://en.bitcoin.it/wiki/Protocol_documentation
@@ -235,21 +284,6 @@ def recvMsg(sock):
 	payload = sockRead(sock, payloadLen)
 	processChunk(header, payload)
 	return command, payload
-	
-# Variable length Integer
-def varint(intData):
-	if intData < 0xfd:
-		return struct.pack('<B', intData)
-	elif intData < 0xffff:
-		return struct.pack('<cH', '\xfd', intData)
-	elif intData < 0xffffffff:
-		return struct.pack('<cL', '\xfe', intData)
-	else:
-		return struct.pack('<cQ', '\xff', intData)
-
-# Variable length String
-def varstr(strData):
-	return varint(len(strData)) + strData
 
 def netaddr(ipaddr, port, timestamp=True):
 	services = 1
@@ -292,7 +326,6 @@ if __name__ == '__main__':
 			sock.connect((peer, 8333))
 			sock.send(createVersionMsg())
 			command, payload = recvMsg(sock)
-			print 'Command: %s - Payload: %d -> %s' % (command, len(payload), binascii.hexlify(payload))
 			break
 		except Exception as e:
 			print e
@@ -300,10 +333,6 @@ if __name__ == '__main__':
 	cmd, payload = recvMsg(sock)
 	sock.send(createMsg('verack', ''))
 	
-	#sock.send(createTrxMsg(binascii.unhexlify(signedTrx)))
-	sock.send(createInvMsg('683a674017c7c3137ca3b65cca928c0a8da35dc1252b6f98931d2f3405d8038a'))	
-	while not cmd.decode().startswith('block'):
+	sock.send(createTrxMsg(signedTrx))
+	while 1:
 		cmd, payload = recvMsg(sock)
-
-	print len(payload)
-	print varint(payload[80:])
